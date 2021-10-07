0a5668a435d3ec46d605317a1a29b32e57afae67
Sachin Tiwary <sachinti@buffalo.edu>
2021-10-07 16:15:40 -0400

    test1

--- src/components/landing/HomePage.js
+++ src/components/landing/HomePage.js
@@ -1,12 +1,43 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+import About from "../about/About";
+import gif from '../landing-gif.gif';
+import Footer from "../footer/Footer";
 
 const HomePage = () => {
+
   return (
-    <div className="pl-8">
-      <Navbar />
+    <div>
+      <div className=" pl-4 bg-green-300  " id='home'>
+        <Navbar />
+      </div>
+      <div className="img-text-wrapper">
+
+        <div style={{ backgroundImage: `url(${gif})` ,backgroundSize:'cover',height:'100vh',backgroundRepeat:'no-repeat' }} > 
+          <div className="pl-40 pt-40 flex flex-col ">
+            <h1  className="text-gr4 text-8xl ">Make some NOISE</h1>
+            <button className={`bg-gr2 max-w-max hover:bg-gr3 text-white font-bold py-2 px-4 rounded`}>
+                <p>Click to JAM</p>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="bg-green-200  h-screen w-full" id='about'>
+        <About />
+      </div>
+      <div className="bg-gr4 h-screen pt-40 pl-40 " id='pricing'>
+        <div className="text-4xl   font-mono">
+          Pricing
+        </div>
+        <p className="pt-2">Enjoy premium version at just $14.99 per month!!</p>
+        <p>Aren't you EXCITED to create music? </p>
+        <p className="text-2xl">I am!!</p>
+      </div>
+      <Footer/>
     </div>
+    
   );
 };
+
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Roboto'],
+    },
+    colors:{
+      "gr1":"#006622",
+      "gr2":"#158236",
+      "gr3":"#359E50",
+      "gr4":"#68BC76",
+      "err":"FF00FF"
+    }
   },
   variants: {
     extend: {},
